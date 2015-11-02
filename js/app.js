cd487c0741c30b566ddbb1343861012532c6d268
tbindrup <tbindrup1221@gmail.com>
2015-11-02 08:59:42 -0700

    Really pushing changes this time

--- js/app.js
+++ js/app.js
@@ -1,258 +1,274 @@
-window.googleDocCallback = function () { return true; };
-var emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+window.jsonpCallback = function () { return true; };
 String.prototype.toProperCase = function () {
 	return this.replace(/\b\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
-angular.module('techtransfer',['ui.router','ui.bootstrap','ngAnimate'])
-.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/');
+(function(app) {
+	// ROUTING
+	function Routes($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
 
-	$stateProvider
-		.state('home', {
-			url: '/',
-			templateUrl: 'templates/home.html',
-			controller: 'HomeController as hmc'
-		})
-
-		.state('about', {
-			url: '/about',
-			templateUrl: 'templates/about.html',
-			controller: 'AboutController as ac'
-		})
-
-		.state('contact', {
-			url: '/contact/{tech_id}',
-			templateUrl: 'templates/contact.html',
-			controller: 'ContactController as cc'
-		})
-
-		.state('technologies', {
-			url: '/technologies',
-			templateUrl: 'templates/technologies.html',
-			controller: 'TechnologiesController as tc'
-		})
-			.state('technology', {
-				url: '/technology/{tech_id}',
-				templateUrl: 'templates/technology.html',
-				controller: 'TechnologyController as stc'
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'templates/home.html',
+				controller: 'HomeController as hmc'
 			})
 
-		.state('resources', {
-			url: '/resources',
-			templateUrl: 'templates/resources.html',
-			controller: 'ResourcesController as rc'
-		});
-})
-.run(function($rootScope, TechnologyDetails) {
-	TechnologyDetails.getTechs().then(function(data) {
-		$rootScope.technologies = data.technologies;
-		$rootScope.categories = data.categories;
-		$rootScope.categorySearch = {'Categories':'Show All'};
-		console.log(data.categories);
-	});
-})
+			.state('about', {
+				url: '/about',
+				templateUrl: 'templates/about.html',
+				controller: 'AboutController as ac'
+			})
 
-.controller('HomeController', function($scope, $state, $window) {
-	var hmc = this;
+			.state('contact', {
+				url: '/contact/{tech_id}',
+				templateUrl: 'templates/contact.html',
+				controller: 'ContactController as cc'
+			})
 
-	// ---------- Video Overlay ------------
-	// -------------------------------------
-	$scope.windowWidth = $window.innerWidth;
-	$scope.jumboHeight = $("video:first").height() > $("img:first").height() ? $("video:first").height() : $("img:first").height();
-	// Watch for changes in the window width
-	$(window).on("resize.doResize", function (){
-		$scope.$apply(function(){
-			$scope.windowWidth = $window.innerWidth;
-			$scope.jumboHeight = $("video:first").height() > $("img:first").height() ? $("video:first").height() : $("img:first").height();
-		});
-	});
-	$scope.$on("$destroy",function (){
-		// Kill resize listener
-		 $(window).off("resize.doResize");
-	});
-	// -------------------------------------
+			.state('technologies', {
+				url: '/technologies',
+				templateUrl: 'templates/technologies.html',
+				controller: 'TechnologiesController as tc',
+				resolve: {
+					technologies: function(TechnologyDetails) {
+						return TechnologyDetails.checkForTechnologyLoaded();
+					}
 
-	hmc.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('TechnologiesController', function($rootScope, $state) {
-	var tc = this;
+				}
+			})
+				.state('technology', {
+					url: '/technology/{tech_id}',
+					templateUrl: 'templates/technology.html',
+					controller: 'TechnologyController as stc',
+					resolve: {
+						technology: function($stateParams, TechnologyDetails) {
+							return TechnologyDetails.getSingleTechnology($stateParams.tech_id);
+						}
+					}
+				})
 
-	tc.searchText = '';
-
-	tc.goToTech = function(tech_id) {
-		$state.go('technology',{'tech_id':tech_id});
-	};
-
-	tc.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('TechnologyController', function($rootScope, $scope, $state, $stateParams) {
-	var stc = this;
-
-	stc.the_tech = {};
-	$scope.$watch(
-		function(scope){return $rootScope.technologies}, 
-		function(new_value, old_value) {
-			if (new_value) {
-				stc.the_tech = new_value.filter(function(item){return item.ID === $stateParams.tech_id})[0];
-			}
-		}
-	);
-
-	stc.contactAboutTech = function(tech_id) {
-		$state.go('contact',{'tech_id':tech_id});
-	};
-	stc.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('ResourcesController', function($state) {
-	var rc = this;
-	rc.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('ContactController', function($scope, $state, $stateParams, Emailer) {
-	var cc = this;
-	cc.formValid = false;
-	cc.emailSent = false;
-	cc.formData = {
-		name:'',
-		patent_id: $stateParams.tech_id,
-		email:'',
-		message:''
-	};
-
-	$scope.$watchCollection(
-		function watchFormData() {
-			return [cc.formData.name, cc.formData.email, cc.formData.message]
-		},
-		function handleFormDataChange() {
-			if (cc.formData.name && emailRegex.test(cc.formData.email) && cc.formData.message) {
-				cc.formValid = true;
-			} else {
-				cc.formValid = false;
-			}
-		}
-	);
-
-	cc.submitForm = function() {
-		if (cc.formValid) {
-			Emailer.SendContactEmail(cc.formData);
-			cc.emailSent = true;
-		}
-	};
-
-	cc.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('AboutController', function($state) {
-	var ac = this;
-	ac.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-
-.controller('HeaderController', function($state) {
- //	$scope.windowWidth = $window.innerWidth;
- //	$scope.showMenu = false;
- //	// Watch for changes in the window width
-	// $(window).on("resize.doResize", function (){
-	// 	$scope.$apply(function(){
- //			$scope.showMenu = false;
-	// 		$scope.windowWidth = $window.innerWidth;
-	// 	});
-	// });
-	// $scope.$on("$destroy",function (){
-	// 	// Kill resize listener
-	// 	 $(window).off("resize.doResize");
-	// });
-	// // -------------------------------------
-
-	this.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('FooterController', function($state) {
-	this.currentYear = new Date().getFullYear();
-	this.goTo = function(pagename) {
-		$state.go(pagename);
-	};
-})
-.controller('TitleController', function(PageTitle) {
-	var tc = this;
-	tc.title = PageTitle.getTitle;
-})
-
-.factory('Emailer',function($http) {
-	return {
-		SendContactEmail: function(the_data) {
-			return $http({
-				method: 'POST',
-				url: 'utils/send_email.php',
-				headers: {
-					'Content-Type':'application/json'
-				},
-				data: the_data
+			.state('resources', {
+				url: '/resources',
+				templateUrl: 'templates/resources.html',
+				controller: 'ResourcesController as rc'
 			});
-		}
-	}
-})
-.factory('TechnologyDetails',function($http, _) {
-	var parseTechnology = function(tech_object) {
+	};
+
+	// DIRECTIVES
+	function bindVideoSize($window, $timeout, VideoSize) {
 		return {
-			'About the Market': tech_object.gsx$aboutthemarket.$t,
-			'Categories': tech_object.gsx$categories.$t,
-			'Contact Email': tech_object.gsx$contactemail.$t,
-			'Contact Name': tech_object.gsx$contactname.$t,
-			'Contact Phone': tech_object.gsx$contactphone.$t,
-			'ID': tech_object.gsx$id.$t,
-			'Image': tech_object.gsx$image.$t,
-			'Links': tech_object.gsx$links.$t.split(','),
-			'Long Description': tech_object.gsx$longdescription.$t,
-			'Name': tech_object.gsx$name.$t,
-			'PI': tech_object.gsx$pi.$t,
-			'Short Description': tech_object.gsx$shortdescription.$t,
-			'Tags': tech_object.gsx$tags.$t
+			restrict: 'A',
+			replace: false,
+			link: function(scope, element) {
+				function bindSize() {
+					scope.$apply(function() {
+						VideoSize.dimensions.width = element[0].clientWidth;
+						VideoSize.dimensions.height = element[0].clientHeight;
+					});
+				};
+				$window.onresize = bindSize;
+				// Allow current digest loop to finish before setting VideoSize
+				$timeout(bindSize, 0);
+			}
 		};
 	};
-	return {
-		getTechs: function() {
-			return $http.get('https://spreadsheets.google.com/feeds/list/17Tf9_PvDC-fx3-vTHkmopjAndc94ZTXWFp-q0jxJjrM/1/public/values?alt=json-in-script&callback=googleDocCallback').then(function(data){
-				var pre = data.data.replace('// API callback\ngoogleDocCallback(','');
+
+	// CONTROLLERS
+	function GenericController($state) {
+		var gc = this;
+		gc.currentYear = new Date().getFullYear();
+		gc.goTo = function(pagename) {
+			$state.go(pagename);
+		};
+	}
+	function HomeController($state, VideoSize) {
+		var hmc = this;
+		hmc.dimensions = VideoSize.dimensions;
+		hmc.goTo = function(pagename) {
+			$state.go(pagename);
+		};
+	};
+	function TechnologiesController($state, technologies) {
+		var tc = this;
+		tc.techData = technologies;
+		tc.searchText = '';
+		tc.categorySearch = {'Categories':'Show All'};
+		tc.goToTech = function(tech_id) {
+			$state.go('technology',{'tech_id':tech_id});
+		};
+		tc.goTo = function(pagename) {
+			$state.go(pagename);
+		};
+	};
+	function TechnologyController($state, technology) {
+		var stc = this;
+		stc.selectedTech = technology;
+		stc.contactAboutTech = function(tech_id) {
+			$state.go('contact',{'tech_id':tech_id});
+		};
+		stc.goTo = function(pagename) {
+			$state.go(pagename);
+		};
+	};
+	function ContactController($scope, $state, $stateParams, Emailer) {
+		var cc = this;
+		cc.formValid = false;
+		cc.emailSent = false;
+		cc.formData = {
+			name:'',
+			patent_id: $stateParams.tech_id,
+			email:'',
+			message:''
+		};
+
+		$scope.$watchCollection(
+			function watchFormData() {
+				return [cc.formData.name, cc.formData.email, cc.formData.message]
+			},
+			function handleFormDataChange() {
+				if (cc.formData.name && emailRegex.test(cc.formData.email) && cc.formData.message) {
+					cc.formValid = true;
+				} else {
+					cc.formValid = false;
+				}
+			}
+		);
+
+		cc.submitForm = function() {
+			if (cc.formValid) {
+				Emailer.SendContactEmail(cc.formData);
+				cc.emailSent = true;
+			}
+		};
+
+		cc.goTo = function(pagename) {
+			$state.go(pagename);
+		};
+	};
+	function TitleController(PageTitle) {
+		var tc = this;
+		tc.title = PageTitle.getTitle();
+	};
+
+	// SERVICES
+	function Emailer($http) {
+		return {
+			SendContactEmail: function(the_data) {
+				return $http({
+					method: 'POST',
+					url: 'utils/send_email.php',
+					headers: {
+						'Content-Type':'application/json'
+					},
+					data: the_data
+				});
+			}
+		}
+	};
+	function TechnologyDetails($http, _) {
+		var techData = {
+			'technologies': null,
+			'categories': null
+		};
+
+		var parseTechnologyFromGoogleSheets = function(tech_object) {
+			return {
+				'About the Market': tech_object.gsx$aboutthemarket.$t,
+				'Categories': tech_object.gsx$categories.$t.split(','),
+				'Contact Email': tech_object.gsx$contactemail.$t,
+				'Contact Name': tech_object.gsx$contactname.$t,
+				'Contact Phone': tech_object.gsx$contactphone.$t,
+				'ID': tech_object.gsx$id.$t,
+				'Image': tech_object.gsx$image.$t,
+				'Links': tech_object.gsx$links.$t.split(','),
+				'Long Description': tech_object.gsx$longdescription.$t,
+				'Name': tech_object.gsx$name.$t,
+				'PI': tech_object.gsx$pi.$t,
+				'Short Description': tech_object.gsx$shortdescription.$t,
+				'Tags': tech_object.gsx$tags.$t.split(',')
+			};
+		};
+		var getAllTechnologyData = function() {
+			return $http.get('https://spreadsheets.google.com/feeds/list/17Tf9_PvDC-fx3-vTHkmopjAndc94ZTXWFp-q0jxJjrM/1/public/values?alt=json-in-script&callback=jsonpCallback').then(function(data){
+				var pre = data.data.replace('// API callback\njsonpCallback(','');
 				var object = JSON.parse(pre.slice(0,pre.length - 2));
 				var result = [];
 				object.feed.entry.map(function(item){
-					result.push(parseTechnology(item));
+					result.push(parseTechnologyFromGoogleSheets(item));
 				});
 				var categories = result.map(function(technology) {
-					var pre = technology.Categories.split(',');
-					return pre.map(function(category) {
+					return technology.Categories.map(function(category) {
 						return category.toProperCase().trim();
 					});
 				});
 				categories = ['Show All'].concat(_.uniq([].concat.apply([],categories).filter(function(item){return !!item})));
-				return {
-					'technologies':result,
-					'categories':categories
-				};
-			})
-		}
-	};
-})
-.factory('PageTitle',function() {
-	var title = 'BYU Tech Transfer';
-	return {
-		getTitle: function(){return title;},
-		setTitle: function(newTitle){title=newTitle; return title;}
-	};
-})
+				techData.technologies = result;
+				techData.categories = categories;
+				return techData;
+			});
+		};
+		var getSingleTechnology = function(tech_id) {
+			return (
+				techData.technologies ? 
+				techData.technologies.filter(function(item){return item.ID === tech_id})[0] : 
+				getAllTechnologyData().then(function(the_techData) {
+					return the_techData.technologies.filter(function(item){return item.ID === tech_id})[0]
+				})
+			);
+		};
+		var checkForTechnologyLoaded = function() {
+			return (techData.technologies ? techData : getAllTechnologyData() );
+		};
 
-.factory('_',function() {
-	return _;
-});
+		return {
+			'techData': techData,
+			'getSingleTechnology': getSingleTechnology,
+			'checkForTechnologyLoaded': checkForTechnologyLoaded
+		};
+	};
+	function VideoSize($interval) {
+		var dimensions = {
+			'width': null,
+			'height': null
+		};
+
+		return {
+			'dimensions': dimensions
+		};
+	}
+	function PageTitle() {
+		var title = {
+			'text': 'BYU Tech Transfer'
+		};
+		return {
+			getTitle: function(){return title.text;},
+			setTitle: function(newTitle){title.text=newTitle; return title.text;}
+		};
+	};
+
+	// RANDOM GLOBAL UTILITIES FOR APP
+	var emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+	// APP BOOTSTRAPPING
+	app
+	.config(Routes)
+	.directive('bindVideoSize', bindVideoSize)
+	.controller('HomeController', HomeController)
+	.controller('TechnologiesController', TechnologiesController)
+	.controller('TechnologyController', TechnologyController)
+	.controller('ResourcesController', GenericController)
+	.controller('ContactController', ContactController)
+	.controller('AboutController', GenericController)
+	.controller('HeaderController', GenericController)
+	.controller('FooterController', GenericController)
+	.controller('TitleController', TitleController)
+	.factory('Emailer', Emailer)
+	.factory('TechnologyDetails', TechnologyDetails)
+	.factory('VideoSize',VideoSize)
+	.factory('PageTitle', PageTitle)
+	.factory('_',function() {
+		return _;
+	});
+})(angular.module('techtransfer',['ui.router','ui.bootstrap','ngAnimate']));
