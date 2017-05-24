c5b96a22f20db7d7e53331e0dc15d17093d7294b
tbindrup <tbindrup1221@gmail.com>
2017-05-23 18:09:20 -0600

    troubleshot auth endpoint

--- api/auth.php
+++ api/auth.php
@@ -1,22 +1,22 @@
 <?php
+require 'password_compat.php';
 // cors
 header("Access-Control-Allow-Origin: *");
 
 // get the HTTP method
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'), true);
- 
+
 // connect to the mysql database
 $link = mysqli_connect('tech-transfer.byu.edu', 'techtrb5_php', 'BlueHost3760#', 'techtrb5_techs');
-mysqli_set_charset($link,'utf8');
- 
+mysqli_set_charset($link, 'utf8');
+
 // create SQL
-$sqlOne = "select * from users where username = '".$input["username"]."'";
+$sql = "select * from users where username = '" . $input["username"] . "';";
 
 // excecute SQL statement
-$result = mysqli_query($link, $sqlOne);
-$hashedPassword = mysqli_fetch_object($result)["password"];
-echo $hashedPassword;
+$result = mysqli_query($link, $sql);
+$hashedPassword = json_decode(json_encode(mysqli_fetch_object($result)), true)["password"];
 
 if (password_verify($input["password"], $hashedPassword)) {
   echo '{"success": true}';
@@ -24,7 +24,7 @@ if (password_verify($input["password"], $hashedPassword)) {
 else {
   echo '{"success": false}';
 }
- 
+
 // close mysql connection
 mysqli_close($link);
 ?>
